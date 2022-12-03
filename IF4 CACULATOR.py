9b4191db5c287fbc16cf99d9999f2c77c5931132
Bhumi Shah <bhumi6633@gmail.com>
2022-12-02 19:33:20 -0500

    IF4 calculator file

---
+++ IF4 CACULATOR.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 13:49:53 2020
+
+@author: Bhumi Shah
+"""
+
+a=int(input("enter a"))    
+b=int(input("enter b"))
+s=input("enter +,-,/,*")
+if s=='+':
+    print(a+b)
+if s=='*':
+    print(a*b)
+if s=='/':
+    print(a/b)
+    
