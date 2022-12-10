7fa2aea7ebc64335bb8aa8e6ab8010c3bdd74a72
Bhumi Shah <bhumi6633@gmail.com>
2022-12-10 16:01:23 -0500

    triangle pattern python row wise

---
+++ TRIANGLE RIGHT 123 ROW.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 00:02:46 2019
+
+@author: Bhumi Shah
+"""
+
+
+for r in range(1,5):
+    for k in range(5,r,-1):
+        print(end='  ')
+    for c in range(1,r+1):
+        print('*',end=' ')
+    print()        
+   
