27fef2245ab2545f33a092ad0ba154d54b1e408a
Bhumi Shah <bhumi6633@gmail.com>
2023-01-17 17:40:58 -0500

    inverse triangle

---
+++ TRN NUMBER 567 INVERSE.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 10:32:49 2020
+
+@author: USER
+"""
+
+for i in range(1,6):
+    for j in range(6,i,-1):
+        print(i,end=' ')
+    print()
+
